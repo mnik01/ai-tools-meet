9731f4c3c018d262b71acdc01bb823d94f161da4
mnik01 <maksimgrinberg01@gmail.com>
2024-03-25 04:26:56 +0500

    wip

---
+++ components/Gist.vue
@@ -0,0 +1,12 @@
+<template>
+  <iframe
+    id="inlineFrameExample"
+    title="Inline gist"
+    width="690"
+    height="400"
+    frameborder="1"
+    allowfullscreen="true"
+    src="https://gist-iframe.vercel.app/"
+  >
+  </iframe>
+</template>
